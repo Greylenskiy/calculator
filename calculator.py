dfadbce6e67386a4712cda8056a94b65e662ae4a
Сергей Левицкий <nepik_one_tap@mail.ru>
2026-09-24 00:24:25 +0300

    Мой первый калькулятор.

---
+++ calculator.py
@@ -0,0 +1,17 @@
+num_1 = float(input('Введите первое число: '))
+num_2 = float(input('Введите второе число: '))
+oper = input('Что хотите сделать?')
+if oper == '+':
+    result = num_1 + num_2
+elif oper == '-':
+    result = num_1 - num_2
+elif oper == '*':
+    result = num_1 * num_2
+elif oper == '/':
+    if num_2 == 0:
+        result = 'На ноль нельзя делить!'
+    if num_2 != 0:
+        result = num_1 / num_2
+else:
+    result = 'Неизвестная операция'
+print(result)
