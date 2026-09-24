64fae3632a70bc165c83992d4f4038115522446b
Сергей Левицкий <nepik_one_tap@mail.ru>
2026-09-24 19:23:17 +0300

    Исправил трейтий инпут(добавил пробел)

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 num_1 = float(input('Введите первое число: '))
 num_2 = float(input('Введите второе число: '))
-oper = input('Что хотите сделать?')
+oper = input('Что хотите сделать? ')
 if oper == '+':
     result = num_1 + num_2
 elif oper == '-':
